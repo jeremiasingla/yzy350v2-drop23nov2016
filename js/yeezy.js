6ef51d5a4eada75382e1d56dbf8e1a9b98b875d3
jeremiasingla <jeremiasingla@gmail.com>
2023-05-19 20:28:13 -0300

    Update

--- js/yeezy.js
+++ js/yeezy.js
@@ -1,42 +1,55 @@
-let redBtn = document.getElementById("Red");
-let greenBtn = document.getElementById("Green");
-let copperBtn = document.getElementById("Copper");
-let imgChange1 = document.getElementById("imgChange1");
-let imgChange2 = document.getElementById("imgChange2");
-let imgChange3 = document.getElementById("imgChange3");
-let imgChange4 = document.getElementById("imgChange4");
+function changeImages(imgUrls) {
+  imgChange1.src = imgUrls[0];
+  imgChange2.src = imgUrls[1];
+  imgChange3.src = imgUrls[2];
+  imgChange4.src = imgUrls[3];
+}
+
+function setActiveColor(activeElement, inactiveElements) {
+  activeElement.classList.add("active");
+
+  for (const element of inactiveElements) {
+    element.classList.remove("active");
+  }
+}
+
+const imgChange1 = document.getElementById("imgChange1");
+const imgChange2 = document.getElementById("imgChange2");
+const imgChange3 = document.getElementById("imgChange3");
+const imgChange4 = document.getElementById("imgChange4");
+
+const redBtn = document.getElementById("Red");
+const greenBtn = document.getElementById("Green");
+const copperBtn = document.getElementById("Copper");
 
 redBtn.onclick = function () {
-  imgChange1.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red1.jpeg";
-  imgChange2.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red2.jpeg";
-  imgChange3.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red3.jpeg";
-  imgChange4.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red4.jpeg";
+  const redUrls = [
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red1.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red2.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red3.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/red/red4.jpeg",
+  ];
+  changeImages(redUrls);
 };
 
 greenBtn.onclick = function () {
-  imgChange1.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green1.jpeg";
-  imgChange2.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green2.jpeg";
-  imgChange3.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green3.jpeg";
-  imgChange4.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green4.jpeg";
+  const greenUrls = [
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green1.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green2.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green3.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/green/green4.jpeg",
+  ];
+  changeImages(greenUrls);
 };
 
 copperBtn.onclick = function () {
-  imgChange1.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper1.jpeg";
-  imgChange2.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper2.jpeg";
-  imgChange3.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper3.jpeg";
-  imgChange4.src =
-    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper4.jpeg";
+  const copperUrls = [
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper1.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper2.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper3.jpeg",
+    "https://jeremiasingla.github.io/yzy350v2-drop23nov2016/images/copper/copper4.jpeg",
+  ];
+  changeImages(copperUrls);
 };
 
 const colorRed = document.querySelector(".color__picker--red");
@@ -47,31 +60,27 @@ const selectGreen = document.querySelector(".select__color--green");
 const selectCopper = document.querySelector(".select__color--copper");
 
 colorRed.addEventListener("click", () => {
-  colorRed.classList.add("active");
-  colorGreen.classList.remove("active");
-  colorCopper.classList.remove("active");
-  selectRed.classList.remove("hidden");
-  selectGreen.classList.add("hidden");
-  selectCopper.classList.add("hidden");
+  setActiveColor(colorRed, [colorGreen, colorCopper]);
+  setHidden(selectRed, selectGreen, selectCopper);
 });
 
 colorGreen.addEventListener("click", () => {
-  colorGreen.classList.add("active");
-  colorRed.classList.remove("active");
-  colorCopper.classList.remove("active");
-  selectRed.classList.add("hidden");
-  selectGreen.classList.remove("hidden");
-  selectCopper.classList.add("hidden");
+  setActiveColor(colorGreen, [colorRed, colorCopper]);
+  setHidden(selectGreen, selectRed, selectCopper);
 });
 
 colorCopper.addEventListener("click", () => {
-  colorCopper.classList.add("active");
-  colorRed.classList.remove("active");
-  colorGreen.classList.remove("active");
-  selectRed.classList.add("hidden");
-  selectGreen.classList.add("hidden");
-  selectCopper.classList.remove("hidden");
+  setActiveColor(colorCopper, [colorRed, colorGreen]);
+  setHidden(selectCopper, selectRed, selectGreen);
 });
+
+function setHidden(activeElement, ...elements) {
+  activeElement.classList.remove("hidden");
+
+  for (const element of elements) {
+    element.classList.add("hidden");
+  }
+}
 
 $(".owl-carousel").owlCarousel({
   loop: true,
